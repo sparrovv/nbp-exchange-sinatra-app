60cf6f7ce10dc678ff2a0b9646021b9be8cd907a
sparrovv <sparrovv@gmail.com>
2011-11-08 11:47:43 +0100

    Clean some mess, make possible to use dev

--- application.rb
+++ application.rb
@@ -16,9 +16,13 @@ class Application < Sinatra::Base
   end
 
   Mongoid.configure do |config|
+    if ENV['MONGOHQ_URL']
       conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
       uri = URI.parse(ENV['MONGOHQ_URL'])
       config.master = conn.db(uri.path.gsub(/^\//, ''))
+    else
+      config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('nbp_currency_dev')
+    end
   end
 
   get '/' do
